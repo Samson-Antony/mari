22bf584044ffbaf9d9d3367751fdf29b92755f70
Bharath Biju <bharathinfoclg@gmail.com>
2020-04-16 22:04:12 +0530

    Updates

---
+++ precache-manifest.6b2ff7e4d4f5b3bf7d1fcc58f1a054f8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2a1d6ec39b7a2982edb",
+    "url": "/mari/static/css/main.a0789661.chunk.css"
+  },
+  {
+    "revision": "d2a1d6ec39b7a2982edb",
+    "url": "/mari/static/js/main.d2a1d6ec.chunk.js"
+  },
+  {
+    "revision": "2f7c282f3721a4eb92dd",
+    "url": "/mari/static/js/1.2f7c282f.chunk.js"
+  },
+  {
+    "revision": "5a0a391764a84810b556",
+    "url": "/mari/static/js/runtime~main.5a0a3917.js"
+  },
+  {
+    "revision": "5be723f121088656193f19a34a3ec2c5",
+    "url": "/mari/static/media/marilux.5be723f1.jpeg"
+  },
+  {
+    "revision": "8e7b90d722f46409bd90eba600ff8f7a",
+    "url": "/mari/static/media/bike.8e7b90d7.jpg"
+  },
+  {
+    "revision": "3291f8e7153eb4c95ebbfaea19650fca",
+    "url": "/mari/static/media/whathesays.3291f8e7.jpg"
+  },
+  {
+    "revision": "2bce18efb9db6f930243505afd8bd292",
+    "url": "/mari/static/media/whatiknow.2bce18ef.jpg"
+  },
+  {
+    "revision": "4174a9f6756b11f7e4a1a703f367f0b5",
+    "url": "/mari/static/media/artrajini.4174a9f6.png"
+  },
+  {
+    "revision": "27ac59613adef9824fbb2ad8ba7d2734",
+    "url": "/mari/static/media/artsam.27ac5961.jpg"
+  },
+  {
+    "revision": "55aee38d1ec31df22ec412dba8f7de23",
+    "url": "/mari/static/media/artvjs.55aee38d.jpg"
+  },
+  {
+    "revision": "5dbe48a54e2d6aac1d422cd80a329cea",
+    "url": "/mari/static/media/vloger1.5dbe48a5.jpg"
+  },
+  {
+    "revision": "0785a8549339de4bbd0bdb67f0bb0ebd",
+    "url": "/mari/static/media/archiintro.0785a854.jpg"
+  },
+  {
+    "revision": "4328397c12c821e6e09b4f8ac27e3377",
+    "url": "/mari/static/media/archithadam.4328397c.jpg"
+  },
+  {
+    "revision": "940ceb3ae8263bb2f02c6c7715f841a7",
+    "url": "/mari/static/media/archithadam2.940ceb3a.jpg"
+  },
+  {
+    "revision": "8f75bc0f12a56a47fa7aba156ac3337f",
+    "url": "/mari/static/media/londoneye.8f75bc0f.jpg"
+  },
+  {
+    "revision": "9602a7e6652e98825e830ffa65e64294",
+    "url": "/mari/static/media/walk.9602a7e6.jpg"
+  },
+  {
+    "revision": "7698e17cb716d594cf96f9dfc2ed57d5",
+    "url": "/mari/index.html"
+  }
+];
